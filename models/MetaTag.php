3f30a7e82693300ca92b9113eb6d2a9d29ce51ae
Brian Pfretzschner <brian.pfretzschner@gmail.com>
2014-08-02 21:50:19 +0200

    Anderer Tags-Bug

--- models/MetaTag.php
+++ models/MetaTag.php
@@ -6,16 +6,14 @@ use app\components\MetaModel;
 class MetaTag extends MetaModel {
 
 	public function getProducts() {
-		return $this->hasMany(MetaProduct::className(), ['id'=>'tag_id'])
-			->viaTable('{{%product_tag}}', ['product_id' => 'id'])
+		return $this->hasMany(MetaProduct::className(), ['id'=>'product_id'])
+			->viaTable('{{%product_tag}}', ['tag_id' => 'id'])
 			->joinWith('i18n')
 			->orderby('sort');
 	}
 	
 	public function getCount() {
-		return $this->hasMany(MetaProduct::className(), ['id'=>'tag_id'])
-			->viaTable('{{%product_tag}}', ['product_id' => 'id'])
-			->count();
+		return $this->getProducts()->count();
 	}
 
 	protected function getI18nClassname() {
