f8a83e70488b3416f2b9f919184d3865400bc0c7
Brian Pfretzschner <brian.pfretzschner@gmail.com>
2015-08-02 13:39:02 +0200

    Preparations for download thumbnails, not working yet

--- app/widgets/ImageWidget.php
+++ app/widgets/ImageWidget.php
@@ -1,6 +1,7 @@
 <?php
 namespace app\widgets;
 
+use app\models\DownloadMeta;
 use app\models\Image;
 use app\models\MetaImage;
 use WideImage\WideImage;
@@ -14,42 +15,60 @@ class ImageWidget extends Widget
 
 	public static function thumbnail(Image $img)
 	{
-		return self::getImage($img, 120, 120, false);
+		return self::getImage($img->fullPath(), $img->extension, $img->hash, 120, 120, false);
 	}
 
 	public static function medium(Image $img)
 	{
-		return self::getImage($img, 200, 200, false);
+		return self::getImage($img->fullPath(), $img->extension, $img->hash, 200, 200, false);
 	}
 
 	public static function full(Image $img)
 	{
-		return self::getImage($img, 1024, 768, true);
+		return self::getImage($img->fullPath(), $img->extension, $img->hash, 1024, 768, true);
 	}
 
 	public static function frontimage(Image $img)
 	{
-		return self::getImage($img, 1280, 720, false, 8);
+		return self::getImage($img->fullPath(), $img->extension, $img->hash, 1280, 720, false, 8);
 	}
 
-	private static function getImage(Image $img, $x, $y, $overlay, $quality = 9)
+//	public static function fileThumbnail(DownloadMeta $download)
+//	{
+//		$dirpath = self::$ROOT_DIR . '/downloads';
+//		if (!is_dir($dirpath)) {
+//			mkdir($dirpath, 0777, true) || die('ImageWidget::fileThumbnail(): mkdir failed');
+//		}
+//
+//		$filepath = $dirpath . '/'.$download->id.'.jpg';
+//		if (!is_file($filepath)) {
+//			$im = new \Imagick('uploads/files/'.$download->filename.'[1]');
+//			$im->thumbnailImage(50, 200, true, true);
+//			$im->setImageFormat('jpg');
+//			$im->writeimage($filepath);
+//		}
+//
+//		return Url::base() . '/' . $filepath;
+//	}
+
+	private static function getImage($fullPath, $extension, $hash, $x, $y, $overlay, $quality = 9)
 	{
 		$dirpath = self::$ROOT_DIR . '/' . $x . 'x' . $y;
 		if (!is_dir($dirpath)) {
 			mkdir($dirpath, 0777, true) || die('ImageWidget::getImage(): mkdir failed');
 		}
 
-		$filepath = $dirpath . '/' . $img->hash . '.' . $img->extension;
+		$filepath = $dirpath . '/' . $hash . '.' . $extension;
 		if (!is_file($filepath)) {
-			self::createImage($img, $filepath, $x, $y, $overlay, $quality);
+			self::createImage($fullPath, $extension, $filepath, $x, $y, $overlay, $quality);
 		}
 
 		return Url::base() . '/' . $filepath;
 	}
 
-	private static function createImage(Image $img, $targetpath, $x, $y, $overlay, $quality)
+	private static function createImage($fullPath, $extension, $targetpath, $x, $y, $overlay, $quality)
 	{
-		$image = WideImage::load($img->fullPath());
+		$image = WideImage::load($fullPath);
 		$image = $image->resize($x, $y);
 
 		if ($overlay) {
@@ -61,7 +80,7 @@ class ImageWidget extends Widget
 		}
 
 
-		if ($img->extension == 'jpg') $quality = $quality*10;
+		if ($extension == 'jpg') $quality = $quality*10;
 
 		$image->saveToFile($targetpath, $quality);
 	}
