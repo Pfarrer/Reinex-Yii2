3292d8913ca8c0bf01ee2e592655049504a4cb43
Brian Pfretzschner <brian.pfretzschner@gmail.com>
2016-01-06 19:58:36 +0100

    Google Analytics

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -68,6 +68,18 @@ AppAsset::register($this);
 		</footer>
 
 		<?= $this->endBody() ?>
+
+		<?php if (YII_ENV == 'prod'): ?>
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+			ga('create', 'UA-72066350-1', 'auto');
+			ga('send', 'pageview');
+		</script>
+		<?php endif; ?>
+
 	</body>
 </html>
 <?= $this->endPage() ?>
